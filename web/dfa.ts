da6fd0fc7070c3ea204eec8d4ff5bfc49fa4e503
guychuk <guy.moa@gmail.com>
2024-08-14 03:19:06 +0300

    Made the transitions curved, rotated texts, added button to generate random DFA, removed the printing of the DFA as JSON.

--- web/dfa.ts
+++ web/dfa.ts
@@ -1,5 +1,12 @@
-import { Alphabet, AState, makeAState, makeDFA, makeATransition, ATransition, DFA } from "../typescript/automata.js"
+import { Alphabet, AState, makeAState, makeDFA, makeATransition, ATransition, DFA, sameTransition } from "../typescript/automata.js"
 import { drawAutomata } from "./fa-drawer.js"
+import { removeDuplicates } from "../typescript/helpers.js";
+
+const alphabetTA  = document.getElementById("alphabet-in") as HTMLTextAreaElement;
+const statesTA = document.getElementById("states-in") as HTMLTextAreaElement;
+const acceptingTA = document.getElementById("accepting-in") as HTMLTextAreaElement;
+const initialTA = document.getElementById("initial-in") as HTMLTextAreaElement;
+const transitionsTA = document.getElementById("transitions-in") as HTMLTextAreaElement;
 
 /**
  * Create a DFA from the given input.
@@ -7,18 +14,12 @@ import { drawAutomata } from "./fa-drawer.js"
  * @returns a DFA object.
  */
 function createDFA(): DFA {
-    const alphabetVal = (document.getElementById("alphabet-in") as HTMLTextAreaElement).value;
-    const statesVal = (document.getElementById("states-in") as HTMLTextAreaElement).value;
-    const acceptingVal = (document.getElementById("accepting-in") as HTMLTextAreaElement).value;
-    const initialVal = (document.getElementById("initial-in") as HTMLTextAreaElement).value;
-    const transitionsVal = (document.getElementById("transitions-in") as HTMLTextAreaElement).value.replace(/\s+/g, "");
-
-    const alphabet: Alphabet = alphabetVal.length === 0 ? [] : alphabetVal.split(',').map(char => char.trim());
-    const states: AState[] = statesVal.length === 0 ? [] : statesVal.split(',').map(stateName => makeAState(stateName.trim()));
-    const acceptingStates: AState[] = acceptingVal.length === 0 ? [] : acceptingVal.split(',').map(stateName => makeAState(stateName.trim()));
-    const initialState: AState | undefined = initialVal.length === 0 ? undefined : makeAState(initialVal.trim());
-    const transitions: ATransition[] = transitionsVal.length === 0 ? [] :
-        transitionsVal.split('/').map(transition => {
+    const alphabet: Alphabet = alphabetTA.value.length === 0 ? [] : alphabetTA.value.split(',').map(char => char.trim());
+    const states: AState[] = statesTA.value.length === 0 ? [] : statesTA.value.split(',').map(stateName => makeAState(stateName.trim()));
+    const acceptingStates: AState[] = acceptingTA.value.length === 0 ? [] : acceptingTA.value.split(',').map(stateName => makeAState(stateName.trim()));
+    const initialState: AState | undefined = initialTA.value.length === 0 ? undefined : makeAState(initialTA.value.trim());
+    const transitions: ATransition[] = transitionsTA.value.replace(/\s+/g, "").length === 0 ? [] :
+        transitionsTA.value.replace(/\s+/g, "").split('/').map(transition => {
             const args = transition.split(',');
 
             if (args.length !== 3)
@@ -35,18 +36,70 @@ function createDFA(): DFA {
 
 function submit(): void {
     const errorOut = document.getElementById("error-out") as HTMLOutputElement;
-    const dfaOut = document.getElementById("dfa-out") as HTMLOutputElement;
     const canvas = document.getElementById("dfa-canv") as HTMLCanvasElement;
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-    dfaOut.textContent = "";
     errorOut.textContent = "";
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     try {
         const dfa = createDFA();
-        dfaOut.textContent = JSON.stringify(dfa);
+        drawAutomata(dfa, canvas, ctx);
+    } catch (error) {
+        errorOut.textContent = (error instanceof Error) ? error.message.toUpperCase() : "unknown error";
+    }
+}
+
+function randomDFA() : void {
+    const generateNum = (lower: number, upper: number) : number => 
+        Math.floor(lower + Math.random() * (upper - lower + 1));
+
+    var allLetters = "abcdefghijklmnopqrstuvwxyz";
+
+    const alphabet: Alphabet = Array.from({length: generateNum(1, allLetters.length)}, () => {
+        const index = generateNum(0, allLetters.length - 1);
+        const char = allLetters[index];
+        
+        allLetters = allLetters.replace(char, '');
+
+        return char;
+    });
+
+    alphabetTA.value = alphabet.join(",");
+
+    const states: AState[] = Array.from({ length: generateNum(1, 8) }, (_, k) => makeAState("q" + k));
+
+    statesTA.value = states.map(s => s.name).join(",");
+
+    const acceptingStates: AState[] = states.filter(_ => 0.5 <= Math.random());
+
+    acceptingTA.value = acceptingStates.map(s => s.name).join(",");
+
+    const initialState: AState = states[generateNum(0, states.length - 1)];
+
+    initialTA.value = initialState.name;
+
+    const transitions: ATransition[] = Array.from({length: generateNum(0, Math.pow(states.length, 2) - 1)}, () => 
+        makeATransition(
+            states[generateNum(0, states.length - 1)], 
+            alphabet[generateNum(0, alphabet.length - 1)], 
+            states[generateNum(0, states.length - 1)]));
+
+    const transitionSet: ATransition[] = removeDuplicates(transitions, (f, s) => f.src === s.src && f.char === s.char);
+
+    transitionsTA.value = transitionSet.map(t => `${t.src.name},${t.char},${t.dest.name}`).join(" / ");
+
+    const errorOut = document.getElementById("error-out") as HTMLOutputElement;
+    const canvas = document.getElementById("dfa-canv") as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    errorOut.textContent = "";
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    try {
+        const dfa = makeDFA(alphabet, states, acceptingStates, initialState, transitionSet);
         drawAutomata(dfa, canvas, ctx);
     } catch (error) {
         errorOut.textContent = (error instanceof Error) ? error.message.toUpperCase() : "unknown error";
@@ -54,5 +107,7 @@ function submit(): void {
 }
 
 const submitButton = document.getElementById("submit-button") as HTMLButtonElement;
+const randomButton = document.getElementById("random-button") as HTMLButtonElement;
 
 submitButton.addEventListener("click", submit);
+randomButton.addEventListener("click", randomDFA);
--- web/fa-drawer.ts
+++ web/fa-drawer.ts
@@ -1,13 +1,13 @@
-import { AState, DFA, NFA, Character, sameState, isAcceptingState } from "../typescript/automata.js"
+import { AState, DFA, NFA, Character, sameState, isAcceptingState, ATransition, makeATransition } from "../typescript/automata.js"
 
-const OUT_RAD = 40, IN_RAD = 35;
+const OUT_RAD = 30, IN_RAD = 25;
 const TRANSITION_ARROW_HEAD_SIZE = 10;
 const INIT_ARROW_LEN = 20, INIT_ARROW_PAD = 10, INIT_ARROW_HEAD_SIZE = 10;
-const STATE_NAME_FONT = '20px Courier New';
-const TRANSITION_FONT = 'bold 20px Courier New';
-const TRANSITION_CHAR_PAD = 15;
-
-// TODO: self transition
+const STATE_NAME_FONT = '20px Cambria';
+const TRANSITION_FONT = '20px Cambria';
+const TRANSITION_CHAR_PAD = 10;
+const SELF_LOOP_RAD = 15;
+const ELEVATION = 30;
 
 /**
  * draw a state in the automata diagram.
@@ -45,14 +45,73 @@ function drawState(ctx: CanvasRenderingContext2D, x: number, y: number, state: A
 
         // draw the arrowhead
         ctx.beginPath();
-        ctx.moveTo(x - INIT_ARROW_HEAD_SIZE * Math.cos(Math.PI / 3), 
+        ctx.moveTo(x - INIT_ARROW_HEAD_SIZE * Math.cos(Math.PI / 3),
             y - OUT_RAD - INIT_ARROW_PAD - INIT_ARROW_HEAD_SIZE * Math.sin(Math.PI / 3));
-        ctx.lineTo(x + INIT_ARROW_HEAD_SIZE * Math.cos(Math.PI / 3), 
+        ctx.lineTo(x + INIT_ARROW_HEAD_SIZE * Math.cos(Math.PI / 3),
             y - OUT_RAD - INIT_ARROW_PAD - INIT_ARROW_HEAD_SIZE * Math.sin(Math.PI / 3));
         ctx.lineTo(x, y - OUT_RAD - INIT_ARROW_PAD);
         ctx.closePath();
         ctx.fill();
     }
+}
+
+function drawSelfTransition(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, x: number, y: number, char: Character): void {
+    // const loopCenterY = y - OUT_RAD - SELF_LOOP_RAD;
+
+    const centerX = canvas.width / 2, centerY = canvas.height / 2;
+    const angle = Math.atan2(y - centerY, x - centerX);
+    const loopCenterY = y + (SELF_LOOP_RAD + OUT_RAD) * Math.sin(angle);
+    const loopCenterX = x + (SELF_LOOP_RAD + OUT_RAD) * Math.cos(angle);
+
+    const startAngle = 3 / 4 * Math.PI;
+    const endAngle = 1 / 4 * Math.PI;
+
+    // draw the arc
+
+    ctx.beginPath();
+    ctx.arc(loopCenterX, loopCenterY, SELF_LOOP_RAD,
+        Math.PI / 2 + angle + startAngle,
+        Math.PI / 2 + angle + endAngle);
+    ctx.stroke();
+
+    // arrow endpoint
+    const arrowX = loopCenterX + SELF_LOOP_RAD * Math.cos(Math.PI / 2 + angle + endAngle);
+    const arrowY = loopCenterY + SELF_LOOP_RAD * Math.sin(Math.PI / 2 + angle + endAngle);
+
+    // arrowhead
+    const arrowAngle = (Math.PI / 2 + angle + endAngle) + Math.PI / 2;
+
+    ctx.beginPath();
+    ctx.moveTo(arrowX, arrowY);
+    ctx.lineTo(
+        arrowX - TRANSITION_ARROW_HEAD_SIZE * Math.cos(arrowAngle - Math.PI / 6),
+        arrowY - TRANSITION_ARROW_HEAD_SIZE * Math.sin(arrowAngle - Math.PI / 6)
+    );
+    ctx.moveTo(arrowX, arrowY);
+    ctx.lineTo(
+        arrowX - TRANSITION_ARROW_HEAD_SIZE * Math.cos(arrowAngle + Math.PI / 6),
+        arrowY - TRANSITION_ARROW_HEAD_SIZE * Math.sin(arrowAngle + Math.PI / 6)
+    );
+    ctx.stroke();
+
+    // write the letter
+
+    const controlX = x + (2 * SELF_LOOP_RAD + OUT_RAD + TRANSITION_CHAR_PAD) * Math.cos(angle);
+    const controlY = y + (2 * SELF_LOOP_RAD + OUT_RAD + TRANSITION_CHAR_PAD) * Math.sin(angle);
+
+    ctx.save();
+    ctx.translate(controlX, controlY);
+
+    if (angle > Math.PI / 2 || angle < -Math.PI / 2)
+        // the text is "too upside down"
+        ctx.rotate(angle + Math.PI);
+    else
+        ctx.rotate(angle);
+
+    ctx.font = TRANSITION_FONT;
+    ctx.fillText(char === '' ? 'ε' : char, 0, 0); 
+
+    ctx.restore(); 
 }
 
 /**
@@ -64,34 +123,64 @@ function drawState(ctx: CanvasRenderingContext2D, x: number, y: number, state: A
  * @param y2 destination state's circle y coordinate.
  * @param char the char corresponding to the transition.
  */
-function drawTransition(ctx: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number, char: Character): void {
+function drawTransition(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number, char: Character): void {
+    // self loop
+
+    if (x1 === x2 && y1 === y2) {
+        drawSelfTransition(canvas, ctx, x1, y1, char);
+        return;
+    }
+
+    // two different states
+
     const angle = Math.atan2(y2 - y1, x2 - x1);
     const startX = x1 + OUT_RAD * Math.cos(angle), startY = y1 + OUT_RAD * Math.sin(angle);
     const endX = x2 - OUT_RAD * Math.cos(angle), endY = y2 - OUT_RAD * Math.sin(angle);
 
-    // draw the line
+    const midX = (startX + endX) / 2, midY = (startY + endY) / 2;
+    const dx = endX - startX, dy = endY - startY;
+    const perpX = -dy, perpY = dx;
+    
+    // normalize the perpendicular vector
+    const length = Math.sqrt(perpX * perpX + perpY * perpY);
+    const unitPerpX = perpX / length;
+    const unitPerpY = perpY / length;
+
+    // control point for the quadratic curve
+    const controlX = midX + unitPerpX * ELEVATION;
+    const controlY = midY + unitPerpY * ELEVATION;
+
+    // draw the arc
     ctx.beginPath();
     ctx.moveTo(startX, startY);
-    ctx.lineTo(endX, endY);
-    ctx.stroke();
+    ctx.quadraticCurveTo(controlX, controlY, endX, endY);
+    ctx.stroke(); 
 
     // draw the head
     ctx.beginPath();
     ctx.moveTo(endX, endY);
-    ctx.lineTo(endX - TRANSITION_ARROW_HEAD_SIZE * Math.cos(angle - Math.PI / 6), 
+    ctx.lineTo(endX - TRANSITION_ARROW_HEAD_SIZE * Math.cos(angle - Math.PI / 6),
         endY - TRANSITION_ARROW_HEAD_SIZE * Math.sin(angle - Math.PI / 6));
     ctx.moveTo(endX, endY);
-    ctx.lineTo(endX - TRANSITION_ARROW_HEAD_SIZE * Math.cos(angle + Math.PI / 6), 
+    ctx.lineTo(endX - TRANSITION_ARROW_HEAD_SIZE * Math.cos(angle + Math.PI / 6),
         endY - TRANSITION_ARROW_HEAD_SIZE * Math.sin(angle + Math.PI / 6));
     ctx.stroke();
 
     // write the letter
-    ctx.font = TRANSITION_FONT;
 
-    if (char === '')
-        ctx.fillText('ε', (startX + endX) / 2 - TRANSITION_CHAR_PAD, (startY + endY) / 2 - TRANSITION_CHAR_PAD);
+    ctx.save();
+    ctx.translate(controlX, controlY);
+
+    if (angle > Math.PI / 2 || angle < -Math.PI / 2)
+        // the text is "too upside down"
+        ctx.rotate(angle + Math.PI);
     else
-        ctx.fillText(char, (startX + endX) / 2 - TRANSITION_CHAR_PAD, (startY + endY) / 2 - TRANSITION_CHAR_PAD);
+        ctx.rotate(angle);
+
+    ctx.font = TRANSITION_FONT;
+    ctx.fillText(char === '' ? 'ε' : char, 0, 0); 
+
+    ctx.restore(); 
 }
 
 /**
@@ -103,12 +192,16 @@ function drawTransition(ctx: CanvasRenderingContext2D, x1: number, y1: number, x
 function calculatePositions(canvas: HTMLCanvasElement, states: AState[]) {
     const centerX = canvas.width / 2, centerY = canvas.height / 2;
     const rotationPerState = (2 * Math.PI) / states.length;
-    const radius = Math.min(canvas.width, canvas.height) / 2 - OUT_RAD;
+    // const margin = 2 * (OUT_RAD + SELF_LOOP_RAD + TRANSITION_CHAR_PAD);
+    const margin = OUT_RAD + TRANSITION_CHAR_PAD + 2 * SELF_LOOP_RAD + 30;
+    const radius = Math.min(canvas.width, canvas.height) / 2 - margin;
 
-    return states.map((state, index) => 
-        ({ state: state, 
-            x: centerX + radius * Math.cos(index * rotationPerState), 
-            y: centerY + radius * Math.sin(index * rotationPerState)}));
+    return states.map((state, index) =>
+    ({
+        state: state,
+        x: centerX + radius * Math.cos(index * rotationPerState),
+        y: centerY + radius * Math.sin(index * rotationPerState)
+    }));
 }
 
 /**
@@ -118,18 +211,54 @@ function calculatePositions(canvas: HTMLCanvasElement, states: AState[]) {
 export function drawAutomata(fa: DFA | NFA, canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D): void {
     const positions = calculatePositions(canvas, fa.states);
 
+    const sameEnds = (f: ATransition, s: ATransition): boolean => sameState(f.src, s.src) && sameState(f.dest, s.dest);
+
     positions.forEach(stateP => drawState(ctx, stateP.x, stateP.y, stateP.state, fa));
-    
-    fa.transitions.forEach(transiton => {
+
+    var combinedList = [];
+
+    var toSkip: ATransition[] = [];
+
+    for (let i = 0; i < fa.transitions.length; i++) {
+        const curr = fa.transitions[i];
+
+        if (toSkip.indexOf(curr) > -1)
+            continue;
+
+        // find all arrows with same ends as the current
+        const sameArrow = fa.transitions.filter(t => sameEnds(curr, t));
+
+        const combinedTransition = makeATransition(curr.src, 
+            sameArrow.map(t => t.char === '' ? 'ε' : t.char).join(', '), curr.dest);
+
+        toSkip = toSkip.concat(sameArrow);
+
+        // push the combined one
+        combinedList.push(combinedTransition);
+    }
+    // fa.transitions.forEach(transiton => {
+    //     const origin = positions.findIndex(pos => sameState(pos.state, transiton.src));
+    //     const end = positions.findIndex(pos => sameState(pos.state, transiton.dest));
+
+    //     if (origin === -1 || end === -1)
+    //         throw new Error("invalid transitions")  // shouldn't happen
+
+    //     drawTransition(canvas, ctx,
+    //         positions[origin].x, positions[origin].y,
+    //         positions[end].x, positions[end].y,
+    //         transiton.char);
+    // });
+
+    combinedList.forEach(transiton => {
         const origin = positions.findIndex(pos => sameState(pos.state, transiton.src));
         const end = positions.findIndex(pos => sameState(pos.state, transiton.dest));
 
         if (origin === -1 || end === -1)
             throw new Error("invalid transitions")  // shouldn't happen
 
-        drawTransition(ctx, 
-            positions[origin].x, positions[origin].y, 
-            positions[end].x, positions[end].y, 
+        drawTransition(canvas, ctx,
+            positions[origin].x, positions[origin].y,
+            positions[end].x, positions[end].y,
             transiton.char);
     });
 }
