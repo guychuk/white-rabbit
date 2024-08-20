6604621f295f9bfbc3c1bbd6aa861917f75a54e7
guychuk <guy.moa@gmail.com>
2024-08-20 09:51:58 +0300

    Deleted everything.

--- typescript/helpers.ts
+++
@@ -1,9 +0,0 @@
-export const isSubArray = <T>(arr: T[], sub: T[], comp: (f: T, s: T) => boolean): boolean =>
-    sub.reduce((acc: boolean, curr) => acc && (arr.findIndex(item => comp(curr, item)) > -1), true);
-
-export const removeDuplicates = <T>(arr: T[], comp: (f: T, s: T) => boolean): T[] =>
-    arr.filter((item, index) => (arr.findIndex(other => comp(item, other)) === index));
-
-export const checkDuplicates = <T>(arr: T[], comp: (f: T, s: T) => boolean): boolean =>
-    arr.reduce((dup: boolean, curr, index) => 
-        dup || (arr.findIndex(other => comp(curr, other)) !== index), false);
--- typescript/jest.config.ts
+++
@@ -1,9 +0,0 @@
-// jest.config.js
-module.exports = {
-  testMatch: ['**/tests/**/*.test.ts'],
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json']
-};
